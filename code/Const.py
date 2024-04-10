36e072b5c7a146dc889bcc6388ba3a16862ce593
Vinicius Basso <vine.maiden@hotmail.com>
2024-04-10 19:59:12 -0400

    adição do menu completo e verificação de eventos

--- code/Const.py
+++ code/Const.py
@@ -1,3 +1,17 @@
 # constantes sempre em MAIÚSCULA
+
+# C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (203, 146, 15)
+
+# M
+MENU_OPTION = ("NEW GAME 1P",
+               "NEW GAME 2P - COPERATIVE",
+               "NEW GAME 2P - COMPETITIVE",
+               "EXIT")
+
+
+# W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
--- code/Menu.py
+++ code/Menu.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import sys
+
 import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -16,10 +18,38 @@ class Menu:
     def run(self):
         pygame.mixer_music.load('./asset/Menu.mp3')  # ./ antes de asset é para sair da pasta code e entrar em asset
         pygame.mixer_music.play(-1)  # -1 significa rodar indefinidamente
+        menu_option = 0
         while True:
-            self.window.blit(source=self.surf, dest=self.rect)      # blit desenha a tela
-            self.menu_text(50, "Cities", (255, 128, 0), ((WIN_WIDTH / 2), 70))
-            pygame.display.flip()                                   # flip atualiza a tela
+            # desenhar na tela
+            self.window.blit(source=self.surf, dest=self.rect)  # blit desenha a tela
+            self.menu_text(50, "Cities", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "WAR", COLOR_ORANGE, ((WIN_WIDTH / 2), 130))
+
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, (WIN_WIDTH / 2, 200 + 30 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, (WIN_WIDTH / 2, 200 + 30 * i))
+            pygame.display.flip()  # flip atualiza a tela OBS: tem que ficar após o ultimo elemento de desenhar na tela
+
+            # verificar eventos
+            for event in pygame.event.get():  # interação com cliques, teclas
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # aqui fecha o pygame, mas ainda rodaria o resto e daria erro.
+                    sys.exit()  # com o sys exit ele finaliza tudo.
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:    # se a seta para baixo for pressionada
+                        if menu_option < len(MENU_OPTION)-1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:    # se a seta para cima for pressionada
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION)-1
+                    if event.key == pygame.K_RETURN:   # se enter for pressionado
+                        return menu_option
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name='Lucida Sans Typewriter', size=text_size)
